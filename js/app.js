d2a0704ea8aa146ac437923347b7761f0cb26e6b
Alexis Adonis <acadonis@gmail.com>
2019-07-16 18:39:52 +0100

    almost working with limiters

--- js/app.js
+++ js/app.js
@@ -1,20 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
-
   //variables - some could be global ========================================
   const squares = []
-  const width = 20
+  const width = 10
   const grid = document.querySelector('.grid')
   let turretCurrentIndex = null
   const scoreBoard = document.querySelector('.score')
   let score = 0
-
-
-
-  //timerIDs=================================================================
-
-
 
   // create grid ============================================================
   for(let i = 0; i < width * width; i++) {
@@ -53,14 +45,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //add invaders=================================================
   const invadersPosition = [
-    5,6,7,8,9,10,11,12,13,14,
-    25,26,27,28,29,30,31,32,33,34
+    2,3,4,5,6,7,
+    12,13,14,15,16,17
 
   ]
 
-  //setting direction of
+  //setting direction of invaders
 
-  let direction = 1
+  let direction = 0
+  let rightCounter = 0
+  let leftCounter = 0
+
+  let moveRight = 1
+  let down = width
+  let moveLeft = -1
+
 
   function invaders() {
 
@@ -77,21 +76,28 @@ document.addEventListener('DOMContentLoaded', () => {
       squares[invadersPosition[i]].classList.add('invader')
     }
 
-    const rightSide = invadersPosition[invadersPosition.length - 1] % width === width - 1
-    const leftSide = invadersPosition[0] % width === 0
+    //
 
-    if((rightSide && direction === 1) || (leftSide && direction === -1)){
-      direction = width
-    } else if (direction === width) {
-      if (leftSide) direction = 1
-      else direction = -1
+    if(rightCounter < 2){
+      direction = moveRight
+      rightCounter++
+
+    }else if (rightCounter === 2) {
+      direction = down
+      rightCounter = 3
+    //
+    }else if (leftCounter < 4) {
+      direction = moveLeft
+      leftCounter ++
+    }else if (leftCounter === 4){
+      direction = down
+      leftCounter = 3
+      rightCounter = 0
     }
-
   }
 
-
   //need to stop at some point
-  const invaderId = setInterval(invaders, 300)
+  const invaderId = setInterval(invaders, 500)
 
   if(squares[turretCurrentIndex].classList.contains('invader', 'turret')) {
     clearInterval(invaderId)
@@ -114,6 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const hit = invadersPosition.indexOf(missileCurrentIndex)
 
         invadersPosition.splice(hit,1)
+        console.log(invadersPosition)
 
         score++
         scoreBoard.textContent = score
@@ -132,38 +139,37 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // fire bombs
-  let bombId = null
-
-  function fireBomb() {
-
-    const randomIndex = Math.floor(Math.random() * invadersPosition.length)
-    console.log(randomIndex)
-    let bomberIndex = invadersPosition[randomIndex]
-    console.log(bomberIndex)
-
-    function moveBomb() {
-
-      squares[bomberIndex].classList.remove('bomb')
-      squares[bomberIndex+=width].classList.add('bomb')
-
-      if(squares[bomberIndex].classList.contains('turret')) {
-        squares[bomberIndex].classList.remove('bomb', 'turret')
-
-        clearInterval(bombId)
-
-      }
-      if(bomberIndex < width) {
-        clearInterval(bombId)
-        squares[bomberIndex].classList.remove('bomb')
-      }
-
-    }
-    bombId = setInterval(moveBomb, 300)
-  }
-
-const fireBombId = setInterval(fireBomb, 1000)
-
+  // // fire bombs
+  // let bombId = null
+  //
+  // function fireBomb() {
+  //
+  //   const randomIndex = Math.floor(Math.random() * invadersPosition.length)
+  //
+  //   let bomberIndex = invadersPosition[randomIndex]
+  //
+  //
+  //   function moveBomb() {
+  //
+  //     squares[bomberIndex].classList.remove('bomb')
+  //     squares[bomberIndex+=width].classList.add('bomb')
+  //
+  //     if(squares[bomberIndex].classList.contains('turret')) {
+  //       squares[bomberIndex].classList.remove('bomb', 'turret')
+  //
+  //       clearInterval(bombId)
+  //
+  //     }
+  //     if(bomberIndex < width) {
+  //       clearInterval(bombId)
+  //       squares[bomberIndex].classList.remove('bomb')
+  //     }
+  //
+  //   }
+  //   bombId = setInterval(moveBomb, 100)
+//   // }
+//
+// const fireBombId = setInterval(fireBomb, 1000)
   //event listeners=============================================================
 
   document.addEventListener('keyup', fireMissile)
